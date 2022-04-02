1f8027f8280b7d243252dc37bb79e8b35a9d8503
Shashank Santosh Kapare <97380124+ammode@users.noreply.github.com>
2022-04-02 19:30:34 +0530

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,8 @@
+print("Caclculate Your Age Here : ")
+
+born = int(input("Birth Year -->"))
+Current = int(input("Current Year -->"))
+print("Your Current Age is : ")
+all = Current - born
+
+print(all) 
